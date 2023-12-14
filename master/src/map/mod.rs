cefbb05557dc62f5d555ce838bad2577f48956e1
lucaSartore <lucasar.com8@gmail.com>
2023-12-14 17:23:16 +0100

    implement node structure

--- master/src/map/mod.rs
+++ master/src/map/mod.rs
@@ -3,32 +3,13 @@ use std::marker::PhantomData;
 use std::ops::Deref;
 use common_infrastructure::Position;
 use common_infrastructure::devices::{Switch, Train};
-use crate::map::states::{NodeRef, SwitchRef, TrainRef, UnIntiNodeRef, IntiNodeRef, UnIntiTrainRef, IntiTrainRef, UnIntiSwitchRef, IntiSwitchRef};
+use crate::map::references::*;
+use crate::map::states::{InitializedState, MapState, MapStateInitialized, MapStateUninitialized};
 
 pub mod states;
 pub mod references;
 
-pub trait MapState{
-    type NodeRefType: NodeRef;
-    type TrainRefType: TrainRef;
-    type SwitchRefTyp: SwitchRef;
-}
 
-pub struct MapStateUninitialized{}
-impl MapState for MapStateUninitialized{
-    type NodeRefType = UnIntiNodeRef;
-    type TrainRefType = UnIntiTrainRef;
-    type SwitchRefTyp = UnIntiSwitchRef;
-}
-pub struct MapStateInitialized<'a>{
-    r: PhantomData<&'a Map<MapStateInitialized<'a>>>
-    //map: &'a >>
-}
-impl<'a> MapState for MapStateInitialized<'a>{
-    type NodeRefType = IntiNodeRef<'a>;
-    type TrainRefType = IntiTrainRef<'a>;
-    type SwitchRefTyp = IntiSwitchRef<'a>;
-}
 
 pub struct Map<T: MapState>{
     state: PhantomData<T>,
@@ -51,7 +32,69 @@ impl<T: MapState> Map<T> {
 
 pub struct Node<T: MapState>{
     state: PhantomData<T>,
-    position: Position
+    position: Position,
+    adjacent_nodes: AdjacentNodes<T>,
+}
+
+/// On our model a node can have at most 3 adjacent nodes...
+/// so instead of using a vector, we use an enum to represent
+/// to avoid accessing the heap for small vectors.
+enum AdjacentNodes<T: MapState>{
+    None,
+    One([T::NodeRefType;1]),
+    Two([T::NodeRefType;2]),
+    Tree([T::NodeRefType;3]),
+}
+
+impl<'a> AdjacentNodes<MapStateInitialized<'a>>{
+    fn get_adjacent_nodes(&'a self) ->&[IntiNodeRef<'a>]{
+        match self {
+            AdjacentNodes::None => &[],
+            AdjacentNodes::One(nodes) => nodes,
+            AdjacentNodes::Two(nodes) => nodes,
+            AdjacentNodes::Tree(nodes) => nodes,
+        }
+    }
+}
+
+/// This struct represent a link between 2 nodes.
+/// in the real world this is a rail track between 2 stations (aka tag rfid)
+pub struct Link<T: MapState>{
+    state: PhantomData<T>,
+    length: u32,
+    max_speed: u32,
+    node: T::NodeRefType,
+    /// If this track travels through a switch, this is the switch
+    /// it can be used to set the correct pat
+    controller: SwitchControllerOption<T>,
+    direction: Direction,
+}
+
+/// When you are in a node (aka station) you can go only int two directions
+/// this enum represent those 2 directions, forward and backward.
+enum Direction{
+    Forward,
+    Backward,
+}
+
+enum SwitchControllerOption<T: MapState>{
+    NoSwitch,
+    SwitchToSetStraight(T::SwitchRefType),
+    SwitchToSetDiverted(T::SwitchRefType),
+}
+
+impl SwitchControllerOption<MapStateInitialized<'_>> {
+    pub fn set(&self){
+        match self {
+            SwitchControllerOption::NoSwitch => {},
+            SwitchControllerOption::SwitchToSetStraight(switch) => {
+                //switch.set_straight();
+            },
+            SwitchControllerOption::SwitchToSetDiverted(switch) => {
+                //switch.set_diverted();
+            },
+        }
+    }
 }
 
 pub struct TrainController<T: MapState>{
--- master/src/map/states.rs
+++ master/src/map/states.rs
@@ -6,169 +6,41 @@
 // An initialized map has the internal references, so it's easy
 // to use, but can't be serialized and deserialized.
 
-use std::ops::Deref;
-use super::{Map, Node};
 use serde::{Serialize, Deserialize};
-use common_infrastructure::Position;
-use common_infrastructure::devices::{Switch, Train};
-use crate::map::{MapStateInitialized, SwitchController, TrainController};
+use crate::map::Map;
+use crate::map::references::*;
+use std::marker::PhantomData;
 
-
-///////////////////// Definition if the generic states /////////////////////
-
-pub trait State{
+pub trait ReferenceState {
     type InitializedType<'a>: InitializedState<'a>;
     type UninitializedType: UninitializedState;
 }
 
-pub trait UninitializedState: State + Serialize + Deserialize<'static>{
+pub trait UninitializedState: ReferenceState + Serialize + Deserialize<'static>{
     fn initialize<'a>(self, map: &'a Map<MapStateInitialized<'a>>) -> Self::InitializedType<'a>;
 }
 
-pub trait InitializedState<'a>: State{
+pub trait InitializedState<'a>: ReferenceState {
     fn un_initialize(self) -> Self::UninitializedType;
 }
 
-///////////////////// Definition of the NodeRef in his two states ////////////////////////
-
-
-pub trait NodeRef: State{}
-
-#[derive(Serialize, Deserialize, Debug, PartialEq, Eq, Copy, Clone)]
-pub struct UnIntiNodeRef{
-    position: Position
-}
-impl NodeRef for UnIntiNodeRef{}
-impl UninitializedState for UnIntiNodeRef{
-    fn initialize<'a>(self, map: &'a Map<MapStateInitialized<'a>>) -> Self::InitializedType<'a> {
-        let node = map.get_node(self.position);
-        IntiNodeRef{
-            node
-        }
-    }
-}
-impl State for UnIntiNodeRef{
-    type InitializedType<'a> = IntiNodeRef<'a>;
-    type UninitializedType = Self;
+pub trait MapState{
+    type NodeRefType: NodeRef;
+    type TrainRefType: TrainRef;
+    type SwitchRefType: SwitchRef;
 }
 
-
-pub struct IntiNodeRef<'a>{
-    node: &'a Node<MapStateInitialized<'a>>
+pub struct MapStateUninitialized{}
+impl MapState for MapStateUninitialized{
+    type NodeRefType = UnIntiNodeRef;
+    type TrainRefType = UnIntiTrainRef;
+    type SwitchRefType = UnIntiSwitchRef;
 }
-impl<'a> NodeRef for IntiNodeRef<'a>{}
-impl<'a> Deref for IntiNodeRef<'a>{
-    type Target = Node<MapStateInitialized<'a>>;
-    fn deref(&self) -> &Self::Target {
-        self.node
-    }
+pub struct MapStateInitialized<'a>{
+    lifetime : PhantomData<&'a Map<MapStateInitialized<'a>>>
 }
-impl<'a> InitializedState<'a> for IntiNodeRef<'a>{
-    fn un_initialize(self) -> Self::UninitializedType {
-        UnIntiNodeRef{
-            position: self.position
-        }
-    }
-}
-impl State for IntiNodeRef<'_>{
-    type InitializedType<'b> = IntiNodeRef<'b>;
-    type UninitializedType = UnIntiNodeRef;
-}
-
-
-///////////////////// Definition of the TrainRef in his two states ////////////////////////
-
-pub trait TrainRef: State{}
-
-#[derive(Serialize, Deserialize, Debug, PartialEq, Eq, Copy, Clone)]
-pub struct UnIntiTrainRef{
-    train: Train
-}
-impl TrainRef for UnIntiTrainRef{}
-
-impl UninitializedState for UnIntiTrainRef{
-    fn initialize<'a>(self, map: &'a Map<MapStateInitialized<'a>>) -> Self::InitializedType<'a> {
-        let train = map.get_train(self.train);
-        IntiTrainRef{
-            train
-        }
-    }
-
-}
-impl State for UnIntiTrainRef{
-    type InitializedType<'a> = IntiTrainRef<'a>;
-    type UninitializedType = Self;
-}
-
-pub struct IntiTrainRef<'a>{
-    train: &'a TrainController<MapStateInitialized<'a>>
-}
-
-impl TrainRef for IntiTrainRef<'_>{}
-
-impl<'a> Deref for IntiTrainRef<'a>{
-    type Target = TrainController<MapStateInitialized<'a>>;
-    fn deref(&self) -> &Self::Target {
-        self.train
-    }
-}
-impl InitializedState<'_> for IntiTrainRef<'_>{
-    fn un_initialize(self) -> Self::UninitializedType {
-        UnIntiTrainRef{
-            train: self.train.train
-        }
-    }
-}
-impl State for IntiTrainRef<'_>{
-    type InitializedType<'a> = Self;
-    type UninitializedType = UnIntiTrainRef;
-}
-
-
-///////////////////// Definition of the SwitchRef in his two states ////////////////////////
-
-pub trait SwitchRef: State{}
-
-#[derive(Serialize, Deserialize, Debug, PartialEq, Eq, Copy, Clone)]
-pub struct UnIntiSwitchRef{
-    switch: Switch
-}
-impl SwitchRef for UnIntiSwitchRef{}
-
-impl UninitializedState for UnIntiSwitchRef{
-    fn initialize<'a>(self, map: &'a Map<MapStateInitialized<'a>>) -> Self::InitializedType<'a> {
-        let switch = map.get_switch(self.switch);
-        IntiSwitchRef{
-            switch
-        }
-    }
-
-}
-impl State for UnIntiSwitchRef{
-    type InitializedType<'a> = IntiSwitchRef<'a>;
-    type UninitializedType = Self;
-}
-
-pub struct IntiSwitchRef<'a>{
-    switch: &'a SwitchController<MapStateInitialized<'a>>
-}
-
-impl SwitchRef for IntiSwitchRef<'_>{}
-
-impl<'a> Deref for IntiSwitchRef<'a>{
-    type Target = SwitchController<MapStateInitialized<'a>>;
-    fn deref(&self) -> &Self::Target {
-        self.switch
-    }
-}
-impl InitializedState<'_> for IntiSwitchRef<'_>{
-    fn un_initialize(self) -> Self::UninitializedType {
-        UnIntiSwitchRef{
-            switch: self.switch.switch
-        }
-    }
-}
-impl State for IntiSwitchRef<'_>{
-    type InitializedType<'a> = Self;
-    type UninitializedType = UnIntiSwitchRef;
+impl<'a> MapState for MapStateInitialized<'a>{
+    type NodeRefType = IntiNodeRef<'a>;
+    type TrainRefType = IntiTrainRef<'a>;
+    type SwitchRefType = IntiSwitchRef<'a>;
 }
