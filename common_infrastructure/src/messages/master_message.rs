800d8f3d95de8db9db51d0505fe15c615b58e450
lucaSartore <lucasar.com8@gmail.com>
2023-12-24 11:01:21 +0100

    add heartbeat manager to common infrastructure

--- common_infrastructure/src/messages/master_message.rs
+++ common_infrastructure/src/messages/master_message.rs
@@ -3,7 +3,6 @@ use crate::{devices::*, Position};
 
 #[derive(Clone, Copy, PartialEq, Eq, Debug, Deserialize, Serialize)]
 pub enum MasterMessage {
-    HeartBeatFrom(Device), // for low level controller
-    TrainHasReachedPosition(Train,Position), // for low level controller
-    //SendTrainTo(Train,Position) do add as rust pipe
+    TrainHasReachedPosition(Train,Position),
+    RequestHeartBeat
 }
--- common_infrastructure/src/messages/mod.rs
+++ common_infrastructure/src/messages/mod.rs
@@ -1,7 +1,9 @@
 mod train_messages;
 mod switch_message;
 mod master_message;
+mod heartbeat_manager_messages;
 
 pub use train_messages::TrainMessage;
 pub use switch_message::SwitchMessage;
 pub use master_message::MasterMessage;
+pub use heartbeat_manager_messages::HeartBeatManagerMessage;
