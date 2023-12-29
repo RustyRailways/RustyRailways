6c72fdc905c87d55a9f15eb9e5e8a46814781387
lucaSartore <lucasar.com8@gmail.com>
2023-12-29 15:32:25 +0100

    finish pf&s

--- master/src/path_finder_and_scheduler/mod.rs
+++ master/src/path_finder_and_scheduler/mod.rs
@@ -1,3 +1,4 @@
+use std::thread::sleep;
 use common_infrastructure::hals::MasterHal;
 use crate::high_level_controller::Request;
 use crate::low_level_controller::LowLevelController;
@@ -5,7 +6,11 @@ use crate::map::views::map_controller_view::MapControllerView;
 use crate::map::views::map_factory::MapFactory;
 use crate::map::views::map_navigation_view::MapNavigationView;
 use anyhow::Result;
-
+use common_infrastructure::devices::Train;
+use map_navigation_functions::{path_to_intersection, path_to_move_out_of_the_way, path_to_position, path_to_switch_point};
+use crate::map::views::MapVisualizationView;
+use crate::path_finder_and_scheduler::map_navigation_functions::{find_path_to_move_out_of_the_way, find_path_to_position};
+use crate::path_finder_and_scheduler::map_navigation_functions::path_to_switch_point::find_path_to_switch_point;
 
 mod map_navigation_functions;
 
@@ -13,6 +18,7 @@ pub struct PathFinderAndScheduler<'a, T: MasterHal> {
     hal: &'a T,
     factory: &'a MapFactory,
     map_controller: MapControllerView<'a, T>,
+    map_visualization: MapVisualizationView,
     low_level_controller: LowLevelController<'a, T>,
     requests: Vec<Request>,
 }
@@ -23,6 +29,7 @@ impl<'a, T: MasterHal> PathFinderAndScheduler<'a, T> {
             hal,
             factory,
             map_controller: factory.build_controller_view(hal),
+            map_visualization: factory.build_visualization_view(),
             low_level_controller: LowLevelController::new(hal, factory.build_controller_view(hal)),
             requests: Vec::new(),
         }
@@ -44,7 +51,62 @@ impl<'a, T: MasterHal> PathFinderAndScheduler<'a, T> {
 
     /// Try to execute a request, return true if the request was executed, false otherwise.
     fn execute_request(&mut self, request: Request)-> Result<bool>{
-        todo!()
+        let train = request.train_id;
+        let destination = request.destination;
+
+        let nv = self.factory.build_navigation_view();
+        let path = find_path_to_position(train, destination, &self.map_visualization, &nv, false)?;
+        drop(nv);
+
+        // can execute the request without moving any train
+        if let Some(path) = path{
+            self.low_level_controller.move_train(train, &path)?;
+            return Ok(true);
+        }
+
+        let nv = self.factory.build_navigation_view();
+        let path = find_path_to_position(train, destination, &self.map_visualization, &nv, true)?;
+
+        let path = match path{
+            // can't execute the request
+            None => return Ok(false),
+            // a path was found
+            Some(s) => s
+        };
+
+        let other_train = match train {
+            Train::T1 => Train::T2,
+            Train::T2 => Train::T1
+        };
+
+        let path_to_move_out_of_the_way = find_path_to_move_out_of_the_way(other_train, &path, &self.map_visualization, &nv)?;
+        drop(nv);
+
+        // the other train can move out of the way easily
+        if let Some(path_to_move_out_of_the_way) = path_to_move_out_of_the_way{
+            self.low_level_controller.move_train(other_train, &path_to_move_out_of_the_way)?;
+            self.low_level_controller.move_train(train, &path)?;
+            return Ok(true);
+        }
+
+        let nv = self.factory.build_navigation_view();
+        let path_to_switch_point = find_path_to_switch_point(train, destination, &self.map_visualization, &nv)?;
+
+        let path_to_switch_point = match path_to_switch_point {
+            // can't execute the request
+            None => return Ok(false),
+            Some(path_to_switch_point) => {path_to_switch_point}
+        };
+
+        drop(nv);
+
+        self.low_level_controller.move_train(train, &path_to_switch_point)?;
+
+        // now the train is in a switch, the request should be able to be executed
+        // with this recursive call, since now the second train can move out of the way
+        let r = self.execute_request(request)?;
+        assert!(r);
+        return Ok(true);
     }
 
     /// Try to execute all the requests in the queue, remove the ones that are executed.
