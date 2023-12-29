c7af74e60a62fda89430768ca90e240debc4c4a6
lucaSartore <lucasar.com8@gmail.com>
2023-12-29 14:38:53 +0100

    add navigation functions

---
+++ master/src/path_finder_and_scheduler/map_navigation_functions.rs
@@ -0,0 +1,36 @@
+use common_infrastructure::Position;
+
+pub mod path_to_position;
+pub use path_to_position::get_path_to_position;
+pub mod move_out_of_the_way;
+pub use move_out_of_the_way::move_out_of_the_way;
+
+//pub mod path_to_intersection;
+
+pub mod path_finder;
+#[derive(PartialEq, Eq, Debug)]
+struct OrderedPosition{
+    priority: u32,
+    position: Position,
+}
+
+impl OrderedPosition{
+    fn new(priority: u32, position: Position) -> Self{
+        Self{
+            priority,
+            position,
+        }
+    }
+}
+
+impl PartialOrd for OrderedPosition {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        self.cmp(other).into()
+    }
+}
+
+impl Ord for OrderedPosition {
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        self.priority.cmp(&other.priority)
+    }
+}
