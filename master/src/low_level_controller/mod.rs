205064a51dab23122cc9b7ec1d4e0c418f03038a
federico-peruzzo <f302.federico@gmail.com>
2024-01-22 17:47:07 +0100

    First implementation of LowLevelController

--- master/src/low_level_controller/mod.rs
+++ master/src/low_level_controller/mod.rs
@@ -3,6 +3,9 @@ use common_infrastructure::hals::MasterHal;
 use common_infrastructure::Position;
 use crate::map::views::map_controller_view::MapControllerView;
 use anyhow::Result;
+use common_infrastructure::messages::{MasterMessage, TrainMessage};
+use crate::map::map_creation_object::TrainStatus;
+
 pub struct LowLevelController<'a, T: MasterHal> {
     hal: &'a T,
     map_controller: MapControllerView<'a, T>,
@@ -17,12 +20,57 @@ impl<'a,T:MasterHal> LowLevelController<'a,T> {
     }
 
     pub fn move_train(&mut self, train: Train, stations: &[Position]) -> Result<()>{
+        match self.move_train_wrapper(train, stations) {
+            Ok(()) => Ok(()),
+            Err(error) => {
+                self.map_controller.stop_train(train)?;
+                for position in stations {
+                    self.map_controller.unlock_node(*position)?;
+                }
+                Err(error)
+            }
+        }
+    }
+
+    fn move_train_wrapper(&mut self, train: Train, stations: &[Position]) -> Result<()>{
         println!("moving train {:?} to {:?} path: {:?}", train, stations.last().unwrap(),stations);
-        unsafe {
-            // nota per federico: questa non é l'implementazione corretta, mi serviva soltanto
-            // per testare lo scheduler... cavala pure... Non servirá usare unsafe nell' implementazione
-            // finale
-            self.map_controller.move_train_unchecked(train, *stations.last().unwrap())?;
+        self.map_controller.set_train_status(train, TrainStatus::Moving)?;
+
+        let final_destination = match stations.last() {
+            Some(v) => v,
+            None => return Err(anyhow::anyhow!("You passed an empty slice"))
+        };
+
+        let mut prev_position = &stations[0];
+        for position in &stations[1..] {
+            self.map_controller.lock_node(*prev_position, train)?;
+            self.map_controller.lock_node(*position, train)?;
+            self.map_controller.set_switch_between(*prev_position, *position)?;
+
+            let wanted_train_speed = self.map_controller.get_speed_to_reach(train, *position)?;
+            self.map_controller.move_train(train, *position)?;
+            if position == final_destination {
+                self.hal.send_message_to_train(train, TrainMessage::SetSpeedAndStopAt(wanted_train_speed, *position))?;
+            } else {
+                self.hal.send_message_to_train(train, TrainMessage::SetSpeed(wanted_train_speed))?;
+            }
+
+            let mut option_recv_message = self.hal.get_message()?;
+            while option_recv_message == None {
+                option_recv_message = self.hal.get_message()?;
+            }
+            if let Some(MasterMessage::TrainHasReachedPosition(moving_train, reached_position)) = option_recv_message{
+                if moving_train == train && reached_position == *position {
+                    self.map_controller.unlock_node(*prev_position)?;
+                } else {
+                    self.map_controller.stop_train(train)?;
+                    self.map_controller.stop_train(moving_train)?;
+                }
+            } else {
+                todo!("HeartBeat management");
+            }
+
+            prev_position = position;
         }
         Ok(())
     }
