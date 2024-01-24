a757da98163d81deb99709b405c818d49cdd7434
luca <lucasar.com8@gmail.com>
2024-01-24 16:46:36 +0100

    add train url

--- common_infrastructure/src/urls.rs
+++ common_infrastructure/src/urls.rs
@@ -3,8 +3,8 @@ pub trait HasUrl {
     fn get_url(&self)->&'static str;
 }
 pub const URL_MASTER: &str = "http://192.168.0.100:9000/master_message";
-pub const URL_T1: &str = "http://192.168.1.15/train_message";
-pub const URL_T2: &str = "xxx";
+pub const URL_T1: &str = "http://192.168.0.101/train_message";
+pub const URL_T2: &str = "http://192.168.0.102/train_message";
 pub const URL_S1: &str = "xxx";
 pub const URL_S2: &str = "xxx";
 pub const URL_S3: &str = "xxx";
