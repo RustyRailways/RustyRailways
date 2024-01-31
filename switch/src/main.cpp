f00d435a278a7891e22b05c2e49a90d4441744b8
MrLakige <callegari.michele.mc@gmail.com>
2024-01-31 11:00:07 +0100

    modified:   platformio.ini 	modified:   src/main.cpp

--- switch/src/main.cpp
+++ switch/src/main.cpp
@@ -12,15 +12,62 @@
 #include <ArduinoOTA.h>
 
 #ifndef STASSID
-#define STASSID "Rusty Railways"
-#define STAPSK "rustyrailways"
+    #define STASSID "Rusty Railways"
+    #define STAPSK "rustyrailways"
 #endif
-#define STRAIGHT 180
-#define DIVERTED 20
+
+#if S1
+    #define STRAIGHT 180
+    #define DIVERTED 20
+    #define SWITCH_NAME "s1"
+#endif
+
+#if S2
+   #define STRAIGHT 170
+    #define DIVERTED 10
+    #define SWITCH_NAME "s2"
+#endif
+
+#if S3
+    #define STRAIGHT 180
+    #define DIVERTED 0
+    #define SWITCH_NAME "s3"
+#endif
+
+#if S4
+   #define STRAIGHT 175
+    #define DIVERTED 5
+    #define SWITCH_NAME "s4"
+#endif
+
+#if S5
+    #define STRAIGHT 170
+    #define DIVERTED 15
+    #define SWITCH_NAME "s5"
+#endif
+
+
+#if S6
+    #define STRAIGHT 175
+    #define DIVERTED 5
+    #define SWITCH_NAME "s6"
+#endif
+
+#ifndef STRAIGHT
+    #define STRAIGHT 180
+#endif
+
+#ifndef DIVERTED
+    #define DIVERTED 20
+#endif
+
+#ifndef SWITCH_NAME
+    #define SWITCH_NAME "general switch"
+#endif
 
 #define JSON_DIM 256 // Adjust the size based on your JSON message, default 256
 
-const char* host = "s1";
+const char* host = SWITCH_NAME;
 const char* ssid = STASSID;
 const char* password = STAPSK;
 
