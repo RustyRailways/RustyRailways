cce89576b39fba375fd01e8c647aa246503ea405
Michele Callegari <michele.callegari@studenti.unitn.it>
2024-02-01 15:34:09 +0100

    Delete doc/cpp_part directory

--- doc/cpp_part/html/search/all_0.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['closeswitch_0',['closeSwitch',['../main_8cpp.html#ab4f9c7a6aafe1e1d2b16d529682e9bee',1,'main.cpp']]]
-];
--- doc/cpp_part/html/search/functions_4.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['setup_13',['setup',['../main_8cpp.html#a4fc01d736fe50cf5b977f755b675f11d',1,'main.cpp']]]
-];
